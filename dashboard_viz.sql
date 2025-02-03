4c2ce4cabd7c96cc1035950396f297daf9d50874
sofa229 <115933945+sofa229@users.noreply.github.com>
2025-02-03 18:51:56 +0100

    Update dashboard_viz.sql

--- dashboard_viz.sql
+++ dashboard_viz.sql
@@ -1,46 +1,49 @@
---1. Top 10 najlepšie hodnotených filmov:
+--1. Priemerné hodnotenia filmov podľa roku vydania:
 SELECT 
-    m.title AS movie_title,
-    DATE_PART('year', m.date_published) AS release_year,
-    r.avg_rating AS average_rating,
-    r.total_votes AS total_votes
-FROM movie m
-JOIN ratings r ON m.id = r.movie_id
-ORDER BY r.avg_rating DESC
+    d.year, 
+    ROUND(AVG(f.avg_rating), 2) AS avg_rating
+FROM fact_ratings f
+JOIN dim_dates d ON f.dim_datesid = d.dim_datesid
+GROUP BY d.year
+ORDER BY d.year;
+
+--2. Najobľúbenejšie filmové žánre podľa hodnotení:
+SELECT 
+    dm.genre, 
+    ROUND(AVG(fr.avg_rating), 2) AS avg_rating
+FROM fact_ratings fr
+JOIN dim_movies dm ON fr.dim_id_movies = dm.dim_id_movies
+GROUP BY dm.genre
+ORDER BY avg_rating DESC
 LIMIT 10;
 
---2. Počet filmov podľa žánru:
+--3. Najlepšie hodnotení režiséri:
 SELECT 
-    g.genre,
-    COUNT(g.movie_id) AS num_movies
-FROM genre g
-GROUP BY g.genre
-ORDER BY num_movies DESC;
+    nd.name AS director_name, 
+    COUNT(fr.dim_id_movies) AS movie_count,
+    ROUND(AVG(fr.avg_rating), 2) AS avg_rating
+FROM fact_ratings fr
+JOIN dim_names_director nd ON fr.director_id = nd.name_id
+GROUP BY nd.name
+HAVING COUNT(fr.dim_id_movies) > 5
+ORDER BY avg_rating DESC
+LIMIT 10;
 
---3. Celkové tržby filmov podľa rokov:
-SELECT 
-    DATE_PART('year', m.date_published) AS year,
-    SUM(TRY_TO_NUMBER(REPLACE(m.worlwide_gross_income, '$', ''))) AS total_gross
-FROM movie m
-WHERE m.worlwide_gross_income IS NOT NULL
-GROUP BY DATE_PART('year', m.date_published)
-ORDER BY year;
 
---4. Počet vydaných filmov podľa rokov:
+--4. Najviac obsadzovaní herci:
 SELECT 
-    DATE_PART('year', m.date_published) AS year,
-    COUNT(m.id) AS num_movies
-FROM movie m
-WHERE m.date_published IS NOT NULL
-GROUP BY DATE_PART('year', m.date_published)
-ORDER BY year;
+    na.name AS actor_name, 
+    COUNT(fr.dim_id_movies) AS movie_count
+FROM fact_ratings fr
+JOIN dim_names_actor na ON fr.actor_id = na.name_id
+GROUP BY na.name
+ORDER BY movie_count DESC
+LIMIT 10;
 
---5. Priemerné hodnotenie filmov podľa žánru:
+--5. Počet filmov podľa roku vydania:
 SELECT 
-    g.genre,
-    AVG(r.avg_rating) AS avg_rating,
-    COUNT(g.movie_id) AS num_movies
-FROM genre g
-JOIN ratings r ON g.movie_id = r.movie_id
-GROUP BY g.genre
-ORDER BY avg_rating DESC;
+    movie_year, 
+    COUNT(*) AS movie_count
+FROM dim_movies
+GROUP BY movie_year
+ORDER BY movie_year;
